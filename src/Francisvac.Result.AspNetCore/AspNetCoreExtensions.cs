408d6e70dcdd037283d81e7b23843bcc0b775ed7
Francis Valoy <francisalcantara001@gmail.com>
2023-11-12 12:52:38 -0400

    refact AspNetCoreExtensions

--- src/Francisvac.Result.AspNetCore/AspNetCoreExtensions.cs
+++ src/Francisvac.Result.AspNetCore/AspNetCoreExtensions.cs
@@ -4,52 +4,34 @@ namespace Francisvac.Result.AspNetCore;
 public static class AspNetCoreExtensions
 {
     public static ActionResult ToActionResult<TData>(this Result<TData> result)
-    {
-        HttpResponseStrategy strategy = result.Status switch
+        => result.Status switch
         {
-            ResultStatus.Success => new OkHttpResponseStrategy(result.Data!, result.Message),
-            ResultStatus.Error => new BadRequestHttpResponseStrategy(result.Message),
-            ResultStatus.NotFound => new NotFoundHttpResponseStrategy(result.Message),
+            ResultStatus.Success => result.Data is null
+                ? new OkObjectResult(result.Message)
+                : new OkObjectResult(result.Data),
+            ResultStatus.Error => new BadRequestObjectResult(result.Message),
+            ResultStatus.NotFound => new NotFoundObjectResult(result.Message),
             _ => throw new InvalidResultStatusException()
         };
-        return strategy.CreateObjectResult();
-    }
 
     public static ActionResult ToActionResult(this Result result)
-    {
-        HttpResponseStrategy strategy = result.Status switch
+        => result.Status switch
         {
-            ResultStatus.Success => new OkHttpResponseStrategy(result.Message),
-            ResultStatus.Error => new BadRequestHttpResponseStrategy(result.Message),
-            ResultStatus.NotFound => new NotFoundHttpResponseStrategy(result.Message),
+            ResultStatus.Success => new OkObjectResult(result.Message),
+            ResultStatus.Error => new BadRequestObjectResult(result.Message),
+            ResultStatus.NotFound => new NotFoundObjectResult(result.Message),
             _ => throw new InvalidResultStatusException()
         };
-        return strategy.CreateObjectResult();
-    }
 
-    public static async Task<ActionResult> ToActionResult<TData>(this Task<Result<TData>> taskResult)
+    public static async Task<ActionResult> ToActionResultAsync<TData>(this Task<Result<TData>> taskResult)
     {
         var r = await taskResult;
-        HttpResponseStrategy strategy = r.Status switch
-        {
-            ResultStatus.Success => new OkHttpResponseStrategy(r.Data!, r.Message),
-            ResultStatus.Error => new BadRequestHttpResponseStrategy(r.Message),
-            ResultStatus.NotFound => new NotFoundHttpResponseStrategy(r.Message),
-            _ => throw new InvalidResultStatusException()
-        };
-        return strategy.CreateObjectResult();
+        return ToActionResult(r);
     }
 
-    public static async Task<ActionResult> ToActionResult(this Task<Result> taskResult)
+    public static async Task<ActionResult> ToActionResultAsync(this Task<Result> taskResult)
     {
         var r = await taskResult;
-        HttpResponseStrategy strategy = r.Status switch
-        {
-            ResultStatus.Success => new OkHttpResponseStrategy(r.Message),
-            ResultStatus.Error => new BadRequestHttpResponseStrategy(r.Message),
-            ResultStatus.NotFound => new NotFoundHttpResponseStrategy(r.Message),
-            _ => throw new InvalidResultStatusException()
-        };
-        return strategy.CreateObjectResult();
+        return ToActionResult(r);
     }
 }
